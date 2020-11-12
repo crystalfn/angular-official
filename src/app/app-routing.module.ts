56e2cb61776703424afc36933b3c2aed0810648d
crystalfn <969274162@qq.com>
2020-11-12 11:29:24 +0800

    [fangni] feat: add router in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CharacterComponent } from './pages/character/character.component';
+import { DocumentsComponent } from './pages/documents/documents.component';
+
+const routes: Routes = [
+  { path: "character", component:  CharacterComponent },
+  { path: "documents", component:  DocumentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
